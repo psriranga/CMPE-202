34e621a05697fe79f08e7f33a0644a5a9adf9603
GRDINESH821 <raghavendradinesh.gurram@sjsu.edu>
2023-10-16 14:49:26 -0700

    Your commit message

--- backend/theater/models.py
+++ backend/theater/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 class Theater(BaseModel):
-    namess = models.CharField(max_length=128)
+    namesss = models.CharField(max_length=128)
     location = PointField()
     address = models.CharField(default="")
     short_address = models.CharField(default="")
