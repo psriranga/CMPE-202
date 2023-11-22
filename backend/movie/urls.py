41a18b8fc5ed6a03960d320e934b2aa16de43f77
GRDINESH821 <raghavendradinesh.gurram@sjsu.edu>
2023-11-21 22:32:33 -0800

    Your commit message

--- backend/movie/urls.py
+++ backend/movie/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from movie.views import MovieListCreateAPI, MovieGetUpdateDeleteAPI
 
 urlpatterns = [
-    path("movies", MovieListCreateAPI.as_view(), name="movie-list-create-api"),
+    path("movivsdes", MovieListCreateAPI.as_view(), name="movie-list-create-api"),
     path("movie/<int:pk>", MovieGetUpdateDeleteAPI.as_view(), name="movie-get-update-delete-api"),
 ]
