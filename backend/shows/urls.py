9f9f3c3815e7bb10a84b66a04c8095788138f394
admin <sriranga.pabbisetty@sjsu.edu>
2023-11-26 15:25:29 -0800

    Movie upcoming and playing now type

--- backend/shows/urls.py
+++ backend/shows/urls.py
@@ -4,6 +4,4 @@ from .views import CreateShowsView, GetShowByIdView
 urlpatterns = [
     path('create/shows', CreateShowsView.as_view(), name='create-shows'),
     path('get-show-by-id/<int:id>/', GetShowByIdView.as_view(), name='get-show-by-id'),
-
-
 ]
