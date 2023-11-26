c2b8eb2bb7acdf7e6f4d7597c482195e2fee195e
admin <sriranga.pabbisetty@sjsu.edu>
2023-11-26 00:12:15 -0800

    Get and post movies done

--- backend/movie/urls.py
+++ backend/movie/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
-from .views import movie_list, movie_detail, CreateMovieView, GetMovieByIdView, GetAllMoviesView
+from movie.views import MovieListCreateAPI, MovieGetAPI
 
 urlpatterns = [
-    path('movies/', movie_list, name='movie_list'),
-    path('movies/<int:movie_id>/', movie_detail, name='movie_detail'),
-    # path('movies/create/', MovieCreateView.as_view(), name='movie_create'),
-    path('movies/create/', CreateMovieView.as_view(), name='create-movie'),
-    path('movies/get-movie/<int:id>/', GetMovieByIdView.as_view(), name='get-movie-by-id'),
-    path('movies/get-all-movies/', GetAllMoviesView.as_view(), name='get-all-movies'),
-
-
-    # Add other URL patterns as needed
+    path("movie", MovieListCreateAPI.as_view(), name="movie-list-create-api"),
+    path("movie/<int:pk>", MovieGetAPI.as_view(), name="movie-get-api"),
 ]
