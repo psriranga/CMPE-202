327acc127a01a259dc09febda1e622adee7f8cf4
admin <sriranga.pabbisetty@sjsu.edu>
2023-11-25 19:44:53 -0800

    Api for get theaters and create theaters

--- backend/theater/models.py
+++ backend/theater/models.py
@@ -7,9 +7,9 @@ from django.db import models
 class Theater(BaseModel):
     name = models.CharField(max_length=128)
     location = PointField()
+    address = models.CharField(default="")
     zip_code = models.CharField(max_length=8)
     technologies = models.JSONField(default=list)
-    seatingCategories = models.JSONField(default=list)
     cuisines = models.JSONField(default=list)
     shows = models.JSONField(default=list)
     no_of_rows = models.IntegerField(default=0)
